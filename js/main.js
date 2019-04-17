6ac5e14831680b0c27dfcc0897857c8423757d68
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-04-17 11:15:51 +0100

    added function to collect and sort reddit posts

--- js/main.js
+++ js/main.js
@@ -3,20 +3,61 @@ sevenToTen = document.getElementById('7to10'),
 tenToFifthteen = document.getElementById('10to15'),
 fifthteenToThirty = document.getElementById('15to30'),
 thirtyPlus = document.getElementById('30plus'),
-videoBoxTemplate = document.getElementById('videoBoxTemplate');
+videoBoxTemplate = document.getElementById('videoboxtemplate'),
+videos = {
+  fiveToSeven: [],
+  sevenToTen: [],
+  tenToFifthteen: [],
+  fifthteenToThirty: [],
+  thirtyPlus:[]
+};
 
-loadVideos();
+getRedditPosts();
 
 function VideoBoxObject(element, postData){
   this.element = element;
 }
 
-function loadVideos(){
-  console.log('hello world');
+function getRedditPosts(){
   let oReq = new XMLHttpRequest();
   oReq.onload = () => {
-    console.log(oReq.responseText);
+    loadVideos(JSON.parse(oReq.responseText).data.children);
   }
-  oReq.open('GET', 'https://www.reddit.com/r/mealtimevideos/top/.json?t=day');
+  oReq.open('GET', 'https://www.reddit.com/r/mealtimevideos/top/.json?t=day&limit=50');
   oReq.send();
+}
+
+function loadVideos(redditPosts) {
+  console.log(redditPosts);
+  for(let i = 0; i < redditPosts.length; i++){
+    let post = redditPosts[i].data;
+    switch (post.link_flair_text) {
+      case '5-7 Minutes':
+        if(videos.fiveToSeven.length < 3){
+          videos.fiveToSeven.push(post);
+        }
+        break;
+      case '7-10 Minutes':
+        if(videos.sevenToTen.length < 3){
+          videos.sevenToTen.push(post);
+        }
+        break;
+      case '10-15 Minutes':
+        if(videos.tenToFifthteen.length < 3){
+          videos.tenToFifthteen.push(post);
+        }
+        break;
+      case '15-30 Minutes':
+        if(videos.fifthteenToThirty.length < 3){
+          videos.fifthteenToThirty.push(post);
+        }
+        break;
+      case '30 Minutes Plus':
+        if(videos.thirtyPlus.length < 3){
+          videos.thirtyPlus.push(post);
+        }
+        break;
+    }
+  }
+  console.log(videos);
 }
