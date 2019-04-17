20039fea928cbefdb5729a9b3522be98161d6568
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-04-17 09:26:50 +0100

    created basic consts and http request

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
-const fiveToSeven = document.getElementById('5to7');
-console.log(fiveToSeven);
+const fiveToSeven = document.getElementById('5to7'),
+sevenToTen = document.getElementById('7to10'),
+tenToFifthteen = document.getElementById('10to15'),
+fifthteenToThirty = document.getElementById('15to30'),
+thirtyPlus = document.getElementById('30plus'),
+videoBoxTemplate = document.getElementById('videoBoxTemplate');
+
+loadVideos();
+
+function VideoBoxObject(element, postData){
+  this.element = element;
+}
+
+function loadVideos(){
+  console.log('hello world');
+  let oReq = new XMLHttpRequest();
+  oReq.onload = () => {
+    console.log(oReq.responseText);
+  }
+  oReq.open('GET', 'https://www.reddit.com/r/mealtimevideos/top/.json?t=day');
+  oReq.send();
+}
